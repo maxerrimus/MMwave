082236b3d3ecd773a554522762759dedcaf63bad
maxerrimus <mass.monteforte@gmail.com>
2016-03-17 11:10:45 +0100

    Update MAIN.cpp

--- MAIN.cpp
+++ MAIN.cpp
@@ -1,5 +1,35 @@
-!*  This is the main file.
+/*  This is the main file.
     From here is possible run the subroutines of the code.
+*/
 
+#include <stdio.h>
 
-*!
+int main(int argc, char *argv[])
+{
+    int i;
+    printf("The following arguments were passed to main(): ");
+    for(i=1; i<argc; i++) printf("%s ", argv[i]);
+    
+    printf("\n");
+    
+    // Boundary condition subroutine
+        
+        // read from file "*.stl" the boundary conditions
+        read_stl();
+    
+    // Initial condition subroutine
+    
+        // read from file "*.start" the initial conditions
+        read_init_cond();
+        
+    // Time marching section 
+        // CFD Solver subroutine
+        
+        // Results writing subroutine 
+        
+            // the hydrodynamic field is written to vtk ".files"
+            
+    // End of Time marching section 
+    
+    return 0;
+} 
